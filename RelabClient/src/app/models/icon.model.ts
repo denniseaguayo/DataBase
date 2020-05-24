916ea7414e0158b7b02f0c47a73523d3db9b75b0
denniseaguayo <aguayo.dennise@iisgalvanimi.edu.it>
2020-05-24 18:28:27 +0000

    Finish mappa con tutti i commenti

--- RelabClient/src/app/models/icon.model.ts
+++ RelabClient/src/app/models/icon.model.ts
@@ -1,15 +1,15 @@
-export class Icon { //Commenta qui
+export class Icon { //Creo la classe Icon
     public scaledSize:ScaledSize;
     constructor(public url: string, size: number){
         this.scaledSize = new ScaledSize(size,size);
     }
 
-    setSize(size: number) { //Commenta qui
+    setSize(size: number) { //li passo la grandezza dell'icona
         this.scaledSize = new ScaledSize(size,size);
     }
 }
 
-export class ScaledSize { //Commenta qui
+export class ScaledSize { //Creo la classe ScaledSize
     constructor(
     public width:  number,
     public height: number){}
